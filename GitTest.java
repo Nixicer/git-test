0a540a75465324677eb9b6689427bee93f40201d
chenyixin <3520952089@qq.com>
2023-04-17 08:26:35 +0800

    first commit

--- GitTest.java
+++ GitTest.java
@@ -1,6 +1,5 @@
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("哈哈");
+        System.out.println("hello git1");
     }
 }
