d121885f2a7910714dc6e59a2bb1f2e6b5d68f85
chenyixin <3520952089@qq.com>
2023-04-18 11:05:12 +0800

    push test

--- GitTest.java
+++ GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("master test1");
         System.out.println("hot-fix test2");
         System.out.println("master test2");
+        System.out.println("push test");
     }
 }
